53fbf77237357d7ca5de0dc659dd37425935dff1
mapleleaf-jian <1028517695@qq.com>
2022-07-13 23:13:27 +0800

    单文件组件

---
+++ 19_单文件组件/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <School></School>
+  </div>
+</template>
+
+<script>
+// 引入组件
+import School from './School'
+
+export default {
+  name: 'App',
+  components: {
+    school
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ 19_单文件组件/School.vue
@@ -0,0 +1,31 @@
+<template>
+  <!--组件的结构-->
+  <div class="demo">
+    <h3>学校地址：{{address}}</h3>
+    <button @click="showInfo">点击展示地址</button>
+  </div>
+</template>
+
+<script>
+  // 组件交互相关的代码(数据、方法等)
+  export default {
+    name: 'School',
+    data() {
+      return {
+        address: 'beijing'
+      }
+    },
+    methods: {
+      showInfo() {
+        alert(this.address)
+      }
+    }
+  }
+</script>
+
+<style>
+  /*组件的样式*/
+  .demo {
+    background-color: aquamarine;
+  }
+</style>
